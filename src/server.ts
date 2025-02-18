346df0214953fbee96228f755b158e0142ade55c
Vagner Nerves <vagnernervessantos@gmail.com>
2025-02-18 16:37:50 -0300

    feat: Instaled zod and fastify-type-provider-zod and configured on server.

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,42 @@
 import { fastify } from "fastify";
 import { fastifyCors } from "@fastify/cors";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
 
-const app = fastify();
+import { z } from "zod";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
 
 app.register(fastifyCors);
 
-app.get("/hello", () => {
-  return "Hello World";
-});
+app.post(
+  "/subscriptions",
+  {
+    schema: {
+      body: z.object({
+        name: z.string(),
+        email: z.string().email(),
+      }),
+      response: {
+        201: z.object({
+          name: z.string(),
+          email: z.string(),
+        }),
+      },
+    },
+  },
+  async (request, reply) => {
+    const { name, email } = request.body;
+
+    return reply.status(201).send({ name, email });
+  }
+);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running!");
