6a7a01b866bbb490b619a5fc663f501a040dd071
Vagner Nerves <vagnernervessantos@gmail.com>
2025-02-18 16:51:25 -0300

    feat: Instaled fastify swagger and fastify swagger ui and configured on server and create path rout with route subscribe.

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 import { fastify } from "fastify";
 import { fastifyCors } from "@fastify/cors";
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
 } from "fastify-type-provider-zod";
 
-import { z } from "zod";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
+
+import { subscribeToEventRoute } from "./routes/subscribe-to-event-route";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -15,28 +19,21 @@ app.setValidatorCompiler(validatorCompiler);
 
 app.register(fastifyCors);
 
-app.post(
-  "/subscriptions",
-  {
-    schema: {
-      body: z.object({
-        name: z.string(),
-        email: z.string().email(),
-      }),
-      response: {
-        201: z.object({
-          name: z.string(),
-          email: z.string(),
-        }),
-      },
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "NLW Connect",
+      version: "0.0.1",
     },
   },
-  async (request, reply) => {
-    const { name, email } = request.body;
+  transform: jsonSchemaTransform,
+});
 
-    return reply.status(201).send({ name, email });
-  }
-);
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
+
+app.register(subscribeToEventRoute);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running!");
