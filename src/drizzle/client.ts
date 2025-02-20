ed0c50dcf5728c8f2f26b988a72b6f7d82fa6b35
Vagner Nerves <vagnernervessantos@gmail.com>
2025-02-19 21:11:32 -0300

    feaT: Configured Drizzle.

--- src/drizzle/client.ts
+++ src/drizzle/client.ts
@@ -2,6 +2,11 @@ import postgres from 'postgres'
 import { env } from '../env'
 
 import { drizzle } from 'drizzle-orm/postgres-js'
+import { subscriptions } from './schema/subscriptions'
 
 export const pg = postgres(env.POSTGRES_URL)
-export const db = drizzle(pg)
+export const db = drizzle(pg, {
+  schema: {
+    subscriptions,
+  },
+})
