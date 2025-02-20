8365b165339ac7db1935b21722bd5f2f18a3cf88
Vagner Nerves <vagnernervessantos@gmail.com>
2025-02-19 21:48:34 -0300

    feat: Create route invites for create invite and redirect and add on server.

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,8 @@ import { fastifySwagger } from '@fastify/swagger'
 import { fastifySwaggerUi } from '@fastify/swagger-ui'
 
 import { env } from './env'
+
+import { accessInviteLinkRoute } from './routes/access-invite-link-route'
 import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -35,6 +37,7 @@ app.register(fastifySwaggerUi, {
 })
 
 app.register(subscribeToEventRoute)
+app.register(accessInviteLinkRoute)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP server running!')
