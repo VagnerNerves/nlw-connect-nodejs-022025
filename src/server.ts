ced12a10c3d051a89f94f475668da22f1df64cc1
Vagner Nerves <vagnernervessantos@gmail.com>
2025-02-20 11:32:05 -0300

    feat: create route get ranking and add server.

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,7 @@ import { fastifySwaggerUi } from '@fastify/swagger-ui'
 import { env } from './env'
 
 import { accessInviteLinkRoute } from './routes/access-invite-link-route'
+import { getRankingRoute } from './routes/get-ranking-route'
 import { getSubscriberInviteClicksRoute } from './routes/get-subscriber-invite-clicks-route'
 import { getSubscriberInvitesCountRoute } from './routes/get-subscriber-invites-count-route'
 import { getSubscriberRankingPositionRoute } from './routes/get-subscriber-ranking-position-route'
@@ -44,6 +45,7 @@ app.register(accessInviteLinkRoute)
 app.register(getSubscriberInviteClicksRoute)
 app.register(getSubscriberInvitesCountRoute)
 app.register(getSubscriberRankingPositionRoute)
+app.register(getRankingRoute)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP server running!')
